e49639d1b08262959e5af98bea6bdf871ec2be52
pitchum <pitchum@users.noreply.github.com>
2017-06-08 19:22:01 +0200

    Ensure PHP required dependencies are present.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -2,6 +2,9 @@
 # Common variables
 #
 
+# Package dependencies
+PKG_DEPENDENCIES="php5-cli php5-mysql php5-json php5-gd php5-tidy php5-curl php-gettext"
+
 # Wallabag version
 VERSION=1.9.2
 
@@ -60,4 +63,48 @@ init_wallabag() {
  
   # install dependencies
   exec_composer "$USER" "$DESTDIR" install --no-dev --prefer-dist
+}
+
+# Define and install dependencies with a equivs control file
+# This helper can/should only be called once per app
+#
+# usage: ynh_install_app_dependencies dep [dep [...]]
+# | arg: dep - the package name to install in dependence
+ynh_install_app_dependencies () {
+    dependencies=$@
+    manifest_path="../manifest.json"
+    if [ ! -e "$manifest_path" ]; then
+    	manifest_path="../settings/manifest.json"	# Into the restore script, the manifest is not at the same place
+    fi
+    version=$(sudo python3 -c "import sys, json;print(json.load(open(\"$manifest_path\"))['version'])")	# Retrieve the version number in the manifest file.
+    dep_app=${app//_/-}	# Replace all '_' by '-'
+
+    if ynh_package_is_installed "${dep_app}-ynh-deps"; then
+		echo "A package named ${dep_app}-ynh-deps is already installed" >&2
+    else
+        cat > ./${dep_app}-ynh-deps.control << EOF	# Make a control file for equivs-build
+Section: misc
+Priority: optional
+Package: ${dep_app}-ynh-deps
+Version: ${version}
+Depends: ${dependencies// /, }
+Architecture: all
+Description: Fake package for ${app} (YunoHost app) dependencies
+ This meta-package is only responsible of installing its dependencies.
+EOF
+        ynh_package_install_from_equivs ./${dep_app}-ynh-deps.control \
+            || ynh_die "Unable to install dependencies"	# Install the fake package and its dependencies
+        ynh_app_setting_set $app apt_dependencies $dependencies
+    fi
+}
+
+
+# Remove fake package and its dependencies
+#
+# Dependencies will removed only if no other package need them.
+#
+# usage: ynh_remove_app_dependencies
+ynh_remove_app_dependencies () {
+    dep_app=${app//_/-}	# Replace all '_' by '-'
+    ynh_package_autoremove ${dep_app}-ynh-deps	# Remove the fake package and its dependencies if they not still used.
 }
